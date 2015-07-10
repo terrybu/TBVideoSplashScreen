1de83551303244e52c80dde53d035e93a030097e
Terry Bu <burea1124@gmail.com>
2015-07-10 14:43:23 -0400

    first commit

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Terry Bu on 7/10/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+import UIKit
+import TBVideoSplashScreen
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
+
